7f5ad7628ec3a34f555d2f0d1b5091e868150d63
Bryan de Jong <bryandejong99@gmail.com>
2021-03-13 12:20:14 +0100

    Change payload jwt type to object'

--- src/auth/tokens/JWTGenerator.ts
+++ src/auth/tokens/JWTGenerator.ts
@@ -2,12 +2,14 @@ import jwt from 'jsonwebtoken';
 import { JWT } from "../JWT";
 
 export class JWTGenerator {
+
   /**
    * Generates a JsonWebToken using the provided payload and secret.
-   * @param payload
+   * @param {object} payload - the payload for the
    * @param secret
+   * @param encrypt
    */
-  static Generate(payload: any, secret: string): JWT {
+  static Generate(payload: object, secret: string): JWT {
     return jwt.sign(JSON.stringify(payload), secret);
   }
 }
