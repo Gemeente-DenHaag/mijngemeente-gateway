2270beb8f16d0a73d8e829a8dc52212dd20b1d55
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-03-17 16:28:44 +0100

    Return bearer in a json body

--- src/routes/AuthenticationRoutes.ts
+++ src/routes/AuthenticationRoutes.ts
@@ -4,5 +4,7 @@ import { MockTokenAuthenticator } from "../auth/MockTokenAuthenticator";
 export const authRouter: Router = express.Router()
   .get("/mock", (req, res) => {
     res.send(
-      new MockTokenAuthenticator().GenerateToken());
+      {
+        token: new MockTokenAuthenticator().GenerateToken()
+      });
   });
