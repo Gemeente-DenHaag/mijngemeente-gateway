6cf425402f9754cc062e3969579096fde96297f6
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-03-24 09:50:54 +0100

    rename merging

--- src/routes/ZaakRoutes.ts
+++ src/routes/ZaakRoutes.ts
@@ -3,7 +3,7 @@ import { VerifyToken } from "../middleware/JWTAuthMiddleware";
 import { zakenHandler } from "./ZakenHandler";
 
 export const ZaakRouter: Router = express.Router()
-    .use(Check)
+    .use(VerifyToken)
     .get("/", zakenHandler)
     .get("/statussen", (req, res) => {
         console.log("hello from /statussen");
