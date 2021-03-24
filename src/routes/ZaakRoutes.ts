1a423efb0d134ec672185b6aa57be95e0062a259
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-03-24 09:50:54 +0100

    Fix forgotten rename

--- src/routes/ZaakRoutes.ts
+++ src/routes/ZaakRoutes.ts
@@ -2,7 +2,7 @@ import express, { Router } from "express";
 import { VerifyToken } from "../middleware/JWTAuthMiddleware";
 
 export const ZaakRouter: Router = express.Router()
-    .use(Check)
+    .use(VerifyToken)
     .get("/statussen", (req, res) => {
         console.log("hello from /statussen");
         res.send(
