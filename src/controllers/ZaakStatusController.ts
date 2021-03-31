2dae83da87c766519c80b9786e14a8a188b8c416
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-03-31 15:28:47 +0200

    Added zaakstatuscontroller

---
+++ src/controllers/ZaakStatusController.ts
@@ -0,0 +1,46 @@
+import { JWT } from "../auth/JWT";
+import got from "got/dist/source";
+import { RequestHandler, Request, Response } from "express";
+import { CreateHeaders } from "../utils/CreateHeader";
+
+interface Query {
+    zaak?: string;
+}
+
+export const ZaakStatusController: RequestHandler = async (
+    req: Request,
+    res: Response
+) => {
+    const openZaakURL = <string>process.env.OPEN_ZAAK_URL;
+    const token: JWT | undefined = req.headers.authorization;
+    const query: Query | undefined = req.query;
+
+    if (token === undefined) {
+        res.status(401).send();
+        return;
+    }
+    const headers = CreateHeaders(token);
+
+    let url: string;
+
+    if (query.zaak !== undefined) {
+        url = `${openZaakURL}/statussen/zaak?=${query.zaak}`;
+    }
+    else {
+        url = `${openZaakURL}/statussen`
+    }
+
+    const options = {
+        headers: headers
+    };
+
+    try {
+        const response = await got(url, options);
+        res.type('json');
+        res.send(response.body);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
--- src/routes/ZaakRoutes.ts
+++ src/routes/ZaakRoutes.ts
@@ -1,13 +1,9 @@
 import express, { Router } from "express";
 import { VerifyToken } from "../middleware/JWTAuthMiddleware";
 import { ZakenController } from "../controllers/ZakenController";
+import { ZaakStatusController } from "../controllers/ZaakStatusController";
 
 export const ZaakRouter: Router = express.Router()
     .use(VerifyToken)
     .get("/", ZakenController)
-    .get("/statussen", (req, res) => {
-        console.log("hello from /statussen");
-        res.send(
-            "test"
-        )
-    });
+    .get("/statussen", ZaakStatusController)
