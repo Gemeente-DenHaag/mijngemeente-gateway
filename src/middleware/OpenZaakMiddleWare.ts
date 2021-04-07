7ec4474b0ff5cb6cc772bd2007d5316169965f87
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-04-07 13:05:27 +0200

    Cleanup of Middleware

--- src/middleware/OpenZaakMiddleWare.ts
+++ src/middleware/OpenZaakMiddleWare.ts
@@ -1,13 +1,13 @@
 import { JWT } from "../auth/JWT";
 import { JWTChecker } from "../auth/tokens/JWTChecker";
-import express from "express";
+import {Request, Response, NextFunction} from "express";
 
-export function OpenZaakMiddleWare(req: express.Request, res: express.Response, next: any) {
+export function OpenZaakMiddleWare(req: Request, res: Response, next: NextFunction): void {
   const openZaakSecret = <string>process.env.OPEN_ZAAK_SECRET;
 
   let jwt: JWT | undefined = req.get("Authorization");
   
-    if (jwt !== null && jwt !== undefined) { 
+    if (jwt) { 
       jwt = jwt.split(" ")[1];
       if (JWTChecker.Verify(jwt, openZaakSecret)) {
         res.type('json');
